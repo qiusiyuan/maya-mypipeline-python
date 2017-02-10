8033d0abec9f7df7fee12af757199a40bfa24565
Qiu Siyuan <siyuan.qiu@mail.utoronto.ca>
2017-02-11 00:38:17 +0900

    Update pipelineMainFunction.py

--- myPipeline/pipelineMainFunction.py
+++ myPipeline/pipelineMainFunction.py
@@ -1,11 +1,14 @@
 ### pipelineMainFunction ###
 import os
 import os.path
+import maya.cmds as cmds
 
 ### ----- openProjectData ------###
 ## this function return a list of projects used pipeline ##
-## Input: None
+## input: None
 ## rtype: List<String>
+## Author : Qiu Siyuan
+## ------------------------------------------------------------##
 def openProjectData(project_path):
     projects_list = []
     for df in os.listdir(project_path):
@@ -18,9 +21,19 @@ def openProjectData(project_path):
 ## input project_path : str
 ## input project_name : str
 ## rtypr : none
+## Author : Qiu Siyuan
+## ---------------------------------------------------------- ##
 def delete_project(project_path, project_name, *pargs):
     for root, dirs, files in os.walk(project_path + project_name, topdown=False):
         for name in files:
             os.remove(os.path.join(root, name))
         for name in dirs:
             os.rmdir(os.path.join(root, name))
+            
+### ----- save_selected ----- ###
+## save currently selected item with given name in the folder(category) user choose ##
+## input item_name : str
+## input folder : str
+## rtype : none
+## Author : Qiu Siyuan
+## -------------------------------------------------------------- ##
